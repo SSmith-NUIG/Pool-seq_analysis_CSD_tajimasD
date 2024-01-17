12eb44ebe732cd27872aece94ff0273a493dc6da
SSmith-NUIG <stephensmith@live.ie>
2024-01-17 16:16:48 +0000

    Update csd_filter.sh

--- csd_filter.sh
+++ csd_filter.sh
@@ -12,17 +12,17 @@ samples_name=$(basename /data3/ssmith/ena/pi/"$SLURM_ARRAY_TASK_ID"_*_csd.pi _cs
 echo $samples_name
 
 # get only the chromosome 1 locations as CSD is on LG1
-grep "LG1" "$samples_name".theta > /data3/ssmith/ena/pi/"$samples_name"_LG1.theta
+grep "LG1" "$samples_name".pi > /data3/ssmith/ena/pi/"$samples_name"_LG1.pi
 
 # get all positions between the start and end of the gene
-awk '$2 > 11771000' /data3/ssmith/ena/pi/"$samples_name"_LG1.theta > /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.theta
+awk '$2 > 11771000' /data3/ssmith/ena/pi/"$samples_name"_LG1.pi > /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.pi
 
-rm /data3/ssmith/ena/pi/"$samples_name"_LG1.theta
+rm /data3/ssmith/ena/pi/"$samples_name"_LG1.pi
 
-awk '$2 < 11781750' /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.theta > /data3/ssmith/ena/pi/"$samples_name"_csd_final.theta
+awk '$2 < 11781750' /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.pi > /data3/ssmith/ena/pi/"$samples_name"_csd_final.pi
 
-rm /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.theta
+rm /data3/ssmith/ena/pi/"$samples_name"_csd_fixed.pi
 
 
 # simply renaming the files
-mv /data3/ssmith/ena/pi/"$samples_name"_csd_final.theta /data3/ssmith/ena/pi/"$samples_name"_csd.theta
+mv /data3/ssmith/ena/pi/"$samples_name"_csd_final.pi /data3/ssmith/ena/pi/"$samples_name"_csd.pi
